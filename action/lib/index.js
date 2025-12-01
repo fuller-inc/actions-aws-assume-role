c936409c1d11aae9fcc2603e09412a352c1d62ee
ICHINOSE Shogo <shogo82148@gmail.com>
2025-12-01 15:56:28 +0900

    bump up to v1.8.0

--- action/lib/index.js
+++ action/lib/index.js
@@ -15,15 +15,25 @@ var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (
 }) : function(o, v) {
     o["default"] = v;
 });
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.assumeRole = void 0;
+exports.assumeRole = assumeRole;
 const core = __importStar(require("@actions/core"));
 const http = __importStar(require("@actions/http-client"));
 function validateGitHubToken(token) {
@@ -113,7 +123,6 @@ async function assumeRole(params) {
     core.exportVariable("AWS_DEFAULT_REGION", params.awsRegion);
     core.exportVariable("AWS_REGION", params.awsRegion);
 }
-exports.assumeRole = assumeRole;
 async function run() {
     try {
         const required = {
